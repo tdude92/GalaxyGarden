42f4f5828df64e4d61eedf096a0059be6ed7a6d4
Cristian Bicheru <c.bicheru0@gmail.com>
2021-09-18 19:41:28 -0400

    patch code into planets

--- app/common/SolarSystem.ts
+++ app/common/SolarSystem.ts
@@ -3,6 +3,13 @@ import { Rendered } from './component/Rendered';
 import { RigidBody } from './component/RigidBody';
 import { Luminous } from './component/Luminous';
 import { genHexstring } from './utils';
+import { random_normal, mulberry32, random_e } from './noise';
+import { HabitablePlanet } from './planet/HabitablePlanet';
+
+const MAX_PLANETS:number = 6;
+const SOLAR_SYSTEM_RADIUS:number = 500;
+const ORBIT_RADIUS_DEVIATION:number = 60;
+const TYPICAL_PLANET_RADIUS:number = 20;
 
 export class SolarSystem {
     /**
@@ -23,11 +30,35 @@ export class SolarSystem {
     skybox_front: THREE.Vector3[][];
     skybox_back:  THREE.Vector3[][];
 
+    rand_fn: () => number;
+
     constructor(name: string, id: string, seed: number) {
         this.name = name;
         this.id = id;
         this.seed = seed;
         // TODO Generate celestial bodies
+        this.rand_fn = mulberry32(seed);
+        let num_planets:number = Math.round(this.rand_fn()*(MAX_PLANETS-1))+1;
+        let prev_apoapsis:number = 0;
+        var orbit_a:number, orbit_e:number, planet_radius:number;
+
+        for (var i = 0; i < num_planets; i++) {
+            planet_radius = Math.abs(random_normal(this.rand_fn)/4+1)*TYPICAL_PLANET_RADIUS;
+
+            while (true) {
+                orbit_a = SOLAR_SYSTEM_RADIUS / 2. / num_planets * (i+1) +
+                          (this.rand_fn() - 0.5) * ORBIT_RADIUS_DEVIATION;
+                orbit_e = random_e(this.rand_fn);
+                if (orbit_a*(1-orbit_e) > prev_apoapsis+planet_radius) {
+                    break;
+                }
+            }
+
+            prev_apoapsis = orbit_a*(1+orbit_e);
+            this.sim_objects.push(new HabitablePlanet(planet_radius, seed, orbit_a, orbit_e, 0, 0))
+        }
+
+        // TODO Generate Sun
         // TODO Generate skybox
     }
 
